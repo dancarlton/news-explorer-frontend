58e42e25d11e0480b01e212fa8dd7b28963ca3cf
Dan Carlton <danlouis.carlton@gmail.com>
2025-07-21 15:32:57 -0700

    wrapped the entire app in BrowserRouter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Main from '../Main/Main'
+import Header from '../Header/Header'
 
 function App() {
 
   return (
     <>
+      <Header />
       <Main />
     </>
   )
